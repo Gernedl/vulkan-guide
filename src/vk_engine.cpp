9bf1f907ddd8ac6ab3b383533ae7fa4aed04f67c
Johannes Gernedl <johannes.gernedl@gmail.com>
2023-05-29 23:53:13 +0200

    print keydown events

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -7,6 +7,8 @@
 #include <vk_types.h>
 #include <vk_initializers.h>
 
+#include <cstdio>
+
 void VulkanEngine::init()
 {
 	// We initialize SDL and create a window with it. 
@@ -50,8 +52,18 @@ void VulkanEngine::run()
 		//Handle events on queue
 		while (SDL_PollEvent(&e) != 0)
 		{
-			//close the window when user alt-f4s or clicks the X button			
-			if (e.type == SDL_QUIT) bQuit = true;
+			switch (e.type)
+			{
+			case SDL_KEYDOWN:
+				printf("Text: %s\n", SDL_GetKeyName(e.key.keysym.sym));
+				break;
+			case SDL_QUIT:
+				//close the window when user alt-f4s or clicks the X button		
+				bQuit = true;
+				break;
+			default:
+				printf("Invalid Event\n");
+			}	
 		}
 
 		draw();
